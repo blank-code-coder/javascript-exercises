3df7920be8e9cb3989e9d888404a752bf9fcd150
zain <23135934+blank-code-coder@users.noreply.github.com>
2024-10-25 20:20:51 +0100

    Create function to add sum between integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
-};
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    }
+    
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
