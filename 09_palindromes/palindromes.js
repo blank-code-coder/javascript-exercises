b20a4342015f2fdad3a65fec8921bda7bb328ee3
zain <23135934+blank-code-coder@users.noreply.github.com>
2025-01-29 00:22:31 +0000

    Create palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+
+for (let i = 0; i < Math.floor(cleanedStr.length / 2); i++) {
+    if (cleanedStr[i] !== cleanedStr[cleanedStr.length - 1 - i]) {
+        return false;
+    }
+}
+return true;
 
 };
 
